00288935735eee5382de30e533ba80adbddd0262
Matthew Brown <mattd429@gmail.com>
2016-12-22 11:46:15 -0500

    Create class.js
    this is a course been working on with JavaScript.

---
+++ class.js
@@ -0,0 +1,30 @@
+console.log(Math.floor(Math.random() * 100));
+
+/*math.random will pick random number to generated followed by a * 'your number(i.e. 100)'
+	math.floor will round the number down to the next whole number.
+	/*
+
+// will be for single line code 
+
+/*this
+is for
+a mulitline
+*/
+
+
+/*
+Let's dissect that statement and look at its parts:
+
+var, short for variable, is the JavaScript keyword that will create a new variable for us.
+myName is chosen by a developer (that's you!). Notice that the word has no spaces, and each new word is capitalized. This is a common convention in JavaScript, and is called camelCase.
+= means to assign whatever's next to the variable.
+'Arya' is the value that the equals = assigns into the variable myName.
+After the variable is declared, we can print the variable with: console.log(myName). This will print 'Arya' to the console.
+*/
+
+var strength = '50,000 pounds'
+console.log('How much stuff can a variable hold? ' , strength)
+var age = 27
+console.log(age)
+var hasPet = true
+console.log(hasPet)
